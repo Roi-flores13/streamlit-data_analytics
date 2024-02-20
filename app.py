60ac6d1be196302b5abdca18bf4b2a214f851bc9
Roi-flores13 <147168985+Roi-flores13@users.noreply.github.com>
2024-02-20 08:25:04 -0600

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ st.title("Hola mundo")
 st.header("Esto es un header")
 st.markdown("### Hola")
 
-path = r"C:\Users\Roi_f\apps\bases-de-datos\src\streamlit\train.csv"
+path = r"train.csv"
 df = pd.read_csv(path)
 
 st.dataframe(df)
